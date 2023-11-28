42920d3712106974c0f147e711c1c7df0b750765
SergioNascimento07 <sergioricjr7@gmail.com>
2023-11-27 21:16:13 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,143 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Card from './Card';
 
 function App() {
+  const [totalDespesa, setTotalDespesa] = useState(0)
+  const [totalReceita, setTotalReceita] = useState(0)
+  const [registrosReceita, setRegistrosReceita] = useState([])
+  const [registrosDespesa, setRegistrosDespesa] = useState([]) 
+  
+  function gerarValorAleatorio() {
+    const parteAleatoria = Math.random().toString(36).substring(2, 15);
+    const timestamp = new Date().getTime();
+    const valorAleatorio = parteAleatoria + timestamp;
+  
+    return valorAleatorio;
+  }
+
+
+  const [dadosFormulario, setDadosFormulario] = useState({
+    nome: '',
+    valor: '',
+    tipo: 'despesa',
+    formaPagamento: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDadosFormulario((prevDados) => ({
+      ...prevDados,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let id = gerarValorAleatorio()
+    // Aqui você pode enviar os dados para o backend ou realizar qualquer outra lógica desejada
+    if (dadosFormulario.tipo === 'despesa') {
+      setTotalDespesa(prev => prev + Number.parseFloat(dadosFormulario.valor))
+      registrosDespesa.push({...dadosFormulario, ...{"id": id}})
+    } else if (dadosFormulario.tipo === 'receita') {
+      setTotalReceita(prev => prev + Number.parseFloat(dadosFormulario.valor))
+      registrosReceita.push({...dadosFormulario, ...{"id": id}})
+    }
+  };
+
+  function handleDeleteRegistro(id, tipo, valor) {
+    if (tipo === 'despesa') {
+      setRegistrosDespesa(
+        prev => prev.filter((value)=> value.id !== id)
+      )
+      setTotalDespesa(prev => prev - valor)
+    } else if (tipo === 'receita') {
+      setRegistrosReceita(
+        prev => prev.filter((value)=> value.id !== id)
+      )
+      setTotalReceita(prev => prev - valor)
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section">
+        <div className='titulos'>
+          <h1>Cadastro</h1>
+        </div>
+        <form onSubmit={handleSubmit}>
+        <label>
+          Nome:
+          <input type="text" name="nome" value={dadosFormulario.nome} onChange={handleChange} />
+        </label>
+
+        <br />
+
+        <label>
+          Valor:
+          <input type="number" name="valor" value={dadosFormulario.valor} onChange={handleChange} />
+        </label>
+
+        <br />
+
+        <label>
+          Tipo:
+          <select name="tipo" value={dadosFormulario.tipo} onChange={handleChange}>
+            <option value="despesa">Despesa</option>
+            <option value="receita">Receita</option>
+          </select>
+        </label>
+
+        <br />
+
+        <label>
+          Forma de Pagamento:
+          <input
+            type="text"
+            name="formaPagamento"
+            value={dadosFormulario.formaPagamento}
+            onChange={handleChange}
+          />
+        </label>
+
+        <br />
+
+        <button type="submit">Enviar</button>
+      </form>
+      </div>
+
+      <div className="section">
+        <div className='titulos'>
+          <h1>Receita</h1>
+          <p>Total: {totalReceita}</p>
+        </div>
+
+        <div className='areacards'>
+          {
+            registrosReceita.map((registro)=> {return (
+              <Card nome={registro.nome} valor={registro.valor} tipo={registro.formaPagamento} onClick={()=>handleDeleteRegistro(registro.id, registro.tipo, registro.valor)}/>
+            )})
+          }
+        </div>
+        
+      </div>
+
+      <div className="section">
+        <div className='titulos'>
+          <h1>Despesa</h1>
+          <p>Total: {totalDespesa}</p>
+        </div>
+
+        <div className='areacards'>
+          {
+            registrosDespesa.map((registro)=> {return (
+              <Card nome={registro.nome} valor={registro.valor} tipo={registro.formaPagamento} onClick={()=>handleDeleteRegistro(registro.id, registro.tipo, registro.valor)}/>
+            )})
+          }
+        </div>
+        
+      </div>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Card = ({id, nome, valor, tipo, onClick}) => {
+
+    return (
+        <div className='card' id={id}>
+        <div className='esquerda'>
+            <h3>Nome: {nome}</h3>
+            <p>Valor: {valor}</p>
+            <p>Pagamento: {tipo}</p>
+        </div>
+        <div className='direita'>
+            <button className='btnDel' onClick={onClick}>Apagar</button>
+        </div>
+
+        </div>
+    )
+}
+
+export default Card
